6bb0068193ab6d09c1c2a4bccbb84fc549ad2b26
Maria Jimenez <mtjimenez@gmail.com>
2022-04-06 22:18:22 -0400

    Query Selectors

--- war.js
+++ war.js
@@ -1,2 +1,17 @@
 console.log("war games not starring matthew broderick");
 
+// let opponentCards = document.querySelectorAll(".opponentCards")
+// console.log('opponentCards');
+// let myCards = document.querySelectorAll(".myCards");
+// console.log("myCards");
+let cards = document.getElementByTagName(div);
+console.log("cards");
+
+
+// function shuffle ()
+// this.opponentCards.sort((a,b) +> Math.random - .5)
+// console.log
+
+
+// function shuffle ()
+// this.myCards.sort((a,b) +> Math.random - .5)
