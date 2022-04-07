3c58b17247d95f82173243e11b78dd916137659f
Maria Jimenez <mtjimenez@gmail.com>
2022-04-07 14:20:11 -0400

    Created an array currentCards to hold 13 cards and values

--- war.js
+++ war.js
@@ -1,4 +1,4 @@
-
+//Create just 10 object cards. Then put them in array. Then go through a for loop of the cards. two arrays? My array and opponent array and have those cards battle each other. 
 
 // document.querySelector('.center').addEventListener("click",  function () {console.log(Hello);});
 
@@ -31,22 +31,95 @@
 //button that has an array of cards for each player and removes the last card from each array as the current card (later)
 
 
-let Card1 = {
-    suit: 'spade',
-    value: 13,
-    image: id="spd13"
-};
 
-console.log(myCard);
 
-let Card2 = {
-    suit: 'diamond',
+
+
+
+
+
+let heart1 = {
+    suit: 'heart',
     value: 1,
-    image: id="dim1"
+    image: id="hrt1"
 };
-console.log(oppCard);
+let heart2 = {
+    suit: 'heart',
+    value: 2,
+    image: id="hrt2"
+};
+let heart3 = {
+    suit: 'heart',
+    value: 3,
+    image: id="hrt3"
+};
+let heart4 = {
+    suit: 'heart',
+    value: 4,
+    image: id="hrt4"
+};
 
-//Create just 10 object cards. Then put them in array. Then go through a for loop of the cards. two arrays? My array and opponent array and have those cards battle each other. 
+let heart5 = {
+    suit: 'heart',
+    value: 5,
+    image: id="hrt5"
+};
+
+let heart6 = {
+    suit: 'heart',
+    value: 6,
+    image: id="hrt6"
+};
+
+let heart7 = {
+    suit: 'heart',
+    value: 7,
+    image: id="hrt7"
+};
+
+let heart8 = {
+    suit: 'heart',
+    value: 8,
+    image: id="hrt8"
+};
+
+let heart9 = {
+    suit: 'heart',
+    value: 9,
+    image: id="hrt9"
+};
+
+let heart10 = {
+    suit: 'heart',
+    value: 10,
+    image: id="hrt10"
+
+};
+
+let heart11 = {
+    suit: 'heart',
+    value: 11,
+    image: id="hrt11"
+
+};
+let heart12 = {
+    suit: 'heart',
+    value: 12,
+    image: id="hrt12"
+
+};
+
+let heart13 = {
+    suit: 'heart',
+    value: 13,
+    image: id="hrt13"
+
+}
+
+
+let currentCards = [heart1.value, heart2.value, heart3.value, heart4.value, heart5.value, heart6.value, heart7.value, heart8.value, heart9.value, heart10.value, heart11.value, heart12.value, heart13.value];
+console.log(currentCards);
+
 
 // let currentCards [
 //     oppCard, mycard]
