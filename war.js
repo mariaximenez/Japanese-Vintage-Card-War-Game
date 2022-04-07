3d40f57dd861eec04e0c670f4b71bcf97c56d36d
Maria Jimenez <mtjimenez@gmail.com>
2022-04-07 11:56:55 -0400

    Preliminary Object assignments

--- war.js
+++ war.js
@@ -1,12 +1,18 @@
-console.log("war games not starring matthew broderick");
+
+
+// document.querySelector('.center').addEventListener("click",  function () {console.log(Hello);});
+
+
+
+
+
 
 // let opponentCards = document.querySelectorAll(".opponentCards")
-// console.log('opponentCards');
+// console.log(opponentCards);
 // let myCards = document.querySelectorAll(".myCards");
-// console.log("myCards");
-let cards = document.getElementByTagName(div);
-console.log("cards");
-
+// console.log(myCards);
+// let cards = document.getElementByTagName(div);
+// console.log("cards");
 
 // function shuffle ()
 // this.opponentCards.sort((a,b) +> Math.random - .5)
@@ -15,3 +21,37 @@ console.log("cards");
 
 // function shuffle ()
 // this.myCards.sort((a,b) +> Math.random - .5)
+
+
+//two objects- each object will have the suit, value, and image
+//store these objects in an array (current cards)
+//function that takes in two cards- card a and card b and comparaes value
+//if card a beats card b, console.log player 1 wins
+//if card a = card b, 
+//button that has an array of cards for each player and removes the last card from each array as the current card (later)
+
+
+let Card1 = {
+    suit: 'spade',
+    value: 13,
+    image: id="spd13"
+};
+
+console.log(myCard);
+
+let Card2 = {
+    suit: 'diamond',
+    value: 1,
+    image: id="dim1"
+};
+console.log(oppCard);
+
+//Create just 10 object cards. Then put them in array. Then go through a for loop of the cards. two arrays? My array and opponent array and have those cards battle each other. 
+
+// let currentCards [
+//     oppCard, mycard]
+// ]
+
+
+
+
